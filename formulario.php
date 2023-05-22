4c6abf9d4648dea1cc179841bdbe78d0242b1315
isaque sene <isaqueconcursos26@gmail.com>
2023-05-22 18:57:11 -0300

    table fila de atendimento

---
+++ formulario.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css_h/estyle.css">
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+
+    <title>ClicaDoc</title>
+</head>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-QK1X3JFN56"></script> <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-QK1X3JFN56'); </script>
+<body id="b">
+    <!--HEADER-->
+    <header>
+        <a href="#" class="logo"><img src="assets/image/logo_icon.png"></a>
+
+        <div class="bx bx-menu" id="menu-icon"></div>
+
+        <ul class="navlist">
+            <li><a href="#saude-do-homem">HOME</a></li>
+            <!--<li><a href="#receita">AGENDAR UMA CONSULTA</a></li>-->
+            <li><a href="#especialistas">MAIS INFORMAÇÕES</a></li>
+            <li><a href="#disfunção_eretil">DISFUNÇÃO ERÉTIL</a></li>
+            <li><a href="#ejaculacao_precoce">EJACULAÇÃO PRECOCE</a></li>
+            <li><a href="#receitas">SERVIÇOS</a></li>
+        </ul>
+        <!--<a href="https://api.whatsapp.com/send?phone=5512935851032&text=Ol%C3%A1+Em+que+Posso+ajudar%3F" target="_blank" class="top-btn-w"><img src="image/WhatsApp.svg.png" alt=""><span class="fale"></span></a>-->
+        <a href="#" id="open-form" target="_blank" data-modal="open" class="top-btn"></a>
+    </header>
+    <!--FIM HEADER-->
+    <section class="card_form">
+        <div class="title">Prontuario Simples</div>
+        <form>
+            <div class="user-details">
+                <div class="input-box">
+                    <span class="details">Data Registro</span>
+                    <input class="input" type="date" name="data_registro" id="data_registro">
+                </div>
+                <div class="input-box">
+                    <span class="details">Nome Completo</span>
+                    <input class="input" type="text" placeholder="Nome Completo" name="nome_completo" id="nome_completo">
+                </div>
+                <div class="input-box">
+                    <span class="details">Idade</span>
+                    <input class="input" type="text" placeholder="Idade" name="idade" id="idade">
+                </div>
+                <div class="input-box">
+                    <span class="details">Data nascimento</span>
+                    <input class="input" type="date" name="data_nasc" id="data_nasc">
+                </div>
+                <div class="input-box">
+                    <span class="details">Endereço</span>
+                    <input class="input" type="text" placeholder="Av, Rua logradouro" name="endereco" id="endereco">
+                </div>
+                <div class="input-box">
+                    <span class="details">Tel / Cel</span>
+                    <input class="input" type="text" placeholder="(**) ****-****" name="tel_cel" id="tel_cel">
+                </div>
+                <div class="input-box">
+                    <span class="details">Sexo</span>
+                    <select class="input" name="sexo" id="sexo">
+                        <option value="">Selecione...</option>
+                        <option value="Masculino">Masculino</option>
+                        <option value="Feminino">Feminino</option>
+                    </select>
+                </div>
+            </div>
+        </form>
+    </section>
+
+    <!--FOOTER-->
+    <footer class="footer">
+        <div class="box-container">
+            <div class="footer-img">
+                <img src="assets/image/logo-footer.png" alt="">
+                <div class="social">
+                    <a href="https://www.facebook.com/profile.php?id=100091349263030"><i class='bx bxl-facebook'></i></a>
+                    <a href=""><i class='bx bxl-youtube'></i></a>
+                    <a href=""><i class='bx bxl-linkedin'></i></a>
+                    <a href=""><i class='bx bxl-instagram'></i></a>
+                </div>
+            </div>
+            
+            <div class="box">
+                <h3>Sobre</h3>
+                <a href="#home"> <i class="fas fa-angle-ri"></i>Home</a>
+                <a href="#"> <i class="fas fa-angle-ri"></i>Parceiros</a>
+                <a href="#"> <i class="fas fa-angle-ri"></i>Termos de Uso</a>
+                <a href="#"> <i class="fas fa-angle-ri"></i>Política de Privacidade</a>
+                <a href="#cookies"> <i class="fas fa-angle-ri"></i>Políticas de Cookies</a>
+            </div>
+
+            <div class="box">
+                <h3>Links úteis</h3>
+                <a href="http://www.planalto.gov.br/ccivil_03/_ato2019-2022/2022/lei/L14510.htm#:~:text=LEI%20Nº%2014.510%2C%20DE%2027,15%20de%20abril%20de%202020"> <i class="fas fa-angle-ri"></i>Lei Telessaúde</a>
+                <a href="https://www.in.gov.br/en/web/dou/-/resolucao-n-727-de-30-de-junho-de-2022-416502055"> <i class="fas fa-angle-ri"></i>Resolução de Telefarmácia</a>
+                <a href="https://www.in.gov.br/en/web/dou/-/resolucao-cfm-n-2.314-de-20-de-abril-de-2022-397602852"> <i class="fas fa-angle-ri"></i>Resolução de Telemedicina</a>
+                <a href="https://site.cfp.org.br/wp-content/uploads/2018/05/RESOLUÇÃO-Nº-11-DE-11-DE-MAIO-DE-2018.pdf"> <i class="fas fa-angle-ri"></i>Resolução de Telepsicologia</a>
+                <a href="http://www.cofen.gov.br/resolucao-cofen-no-696-2022_99117.html"> <i class="fas fa-angle-ri"></i>Resolução de Telenfermagem</a>
+            </div>
+
+            <div class="pague-img">
+                <a href="https://www.iugu.com/"><img src="assets/image/IUGU.png"></a>
+                <a href="https://www.iugu.com/"><img src="assets/image/pague_seguro.png"></a>
+            </div>
+
+            <div class="box">
+                <h3>Trabalhe Conosco</h3>
+                <a href="#"><i class="fab fa-facebook-f"></i>Cadastro médico</a>
+                <a href="#"><i class="fab fa-twitter"></i>Quero ser parceiro</a>
+                <h3>Precisa de Ajuda?</h3>
+                <h2 type="email">contato@clicadoc.com.br</h2>
+            </div>
+
+        </div>
+    </footer>
+
+    <!--footer-->
+    <section class="ends">
+        <div class="credit">CLICADOC TECNOLOGIA  LTDA - CNPJ: 43.722.721/0001-18</div><br>
+
+        <div class="credit-footer"> Desenvolvido por <span>ITA Ventures</span> © 2023 </div>
+    </section>
+
+    <script src="https://unpkg.com/scrollreveal"></script>
+
+    <script src="assets/js_h/script.js"></script>
+
+    <script src="assets/js_h/scroll.js"></script>
+
+    <script src="assets/js_h/cookies.js"></script>
+
+    <!--<script src="js_h/modal.js"></script>
+
+    <script src="js_h/modal2.js"></script>-->
+
+    <script src="assets/js_h/whatsapp.shrink.js"></script>
+
+
+    
+</body>
+</html>

1f3928caa1878c799e2abe5a7387fd06010e2eea
isaque sene <isaqueconcursos26@gmail.com>
2023-05-23 18:20:24 -0300

    comit

--- assets/ajax/atualiza_formulario.php
+++ assets/ajax/atualiza_formulario.php
@@ -3,13 +3,14 @@ include "../../include/valida_session_usuario.php";
 include "../../include/mysqlconecta.php";
 
 if(isset($_POST['submit'])){
-    $anmpac_pagamento_status = $_POST['anmpac_pagamento_status'];
+    $anmpac_med_presc = $_POST['aanmpac_med_presc'];
+    $anmpac_tratamento = $_POST['anmpac_tratamento'];
+    $anmpac_alergia = $_POST['anmpac_alergia'];
     $anmpac_nom = $_POST['anmpac_nom'];
-    $anmpac_cpf = $_POST['anmpac_cpf'];
-    $anmpac_data_cadastro = $_POST['anmpac_data_cadastro'];
+    $anmpac_numcel = $_POST['anmpac_numcel'];
     
-    $SQL = "insert into tanam_dados_pacientes(anmpac_pagamento_status, anmpac_nom, anmpac_cpf, anmpac_data_cadastro) values 
-    ('$anmpac_pagamento_status','$anmpac_nom','$anmpac_cpf','$anmpac_data_cadastro')";    
+    $SQL = "insert into tanam_dados_pacientes(anmpac_med_presc, anmpac_tratamento, anmpac_alergia, anmpac_nom, anmpac_numcel) values 
+    ('$anmpac_med_presc','$anmpac_tratamento','$anmpac_alergia','$anmpac_nom','$anmpac_numcel')";    
     @mysqli_query($conexao,$SQL) or die("Ocorreu um problema! Código: 001");  
 
 }
